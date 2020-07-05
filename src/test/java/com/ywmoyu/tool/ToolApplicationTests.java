cdab50d2ad32955a9e6573db3034cd8ede030b76
YVictor13 <2357772033@qq.com>
2020-07-05 16:11:25 +0800

    初始化项目

---
+++ src/test/java/com/ywmoyu/tool/ToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ywmoyu.tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
